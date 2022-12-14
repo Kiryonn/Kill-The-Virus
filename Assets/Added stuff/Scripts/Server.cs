392770fb54e74b3c6c500bc1fb5a208a9735025f
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2022-12-14 12:59:45 +0100

    add scrip for server

---
+++ Assets/Added stuff/Scripts/Server.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class server : MonoBehaviour
+{
+    public bool isUnderAttack;
+    void Start()
+    {
+        isUnderAttack = false;
+    }
+
+    public void repare() 
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
